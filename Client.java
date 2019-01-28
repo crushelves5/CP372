a0e58316cec0cf17d607a06868ac221498d81658
crushelves5 <37851515+crushelves5@users.noreply.github.com>
2019-01-28 17:37:04 -0500

    Added CheckMessage Class
    Determines if a message is allowed to be sent to the server

--- Client.java
+++ Client.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) throws Exception {
+	
 //        System.out.println("Enter the IP address of a machine running the capitalize server:");
         String serverAddress = "localhost";//new Scanner(System.in).nextLine();
         Socket socket = new Socket(serverAddress, 9898);
@@ -19,15 +20,82 @@ public class Client {
         System.out.println(in.readLine());
 
         Scanner scanner = new Scanner(System.in);
+		Scanner message_scan;
         while (true) {
             System.out.println("\nEnter a message to send to the server (DISCONNECT to disconnect):");
             String message = scanner.nextLine();
-            if (message.equals("DISCONNECT")) {
+			message_scan = new Scanner(message);
+            if (message.equals("DISCONNECT")) {//disconnect from server and client session ends
                 break;
             }
-            out.println(message);
-            System.out.println(in.readLine());
+			else{
+				//Determine if the message is a valid format
+				boolean msg_valid = CheckMessage(message_scan);
+				if (msg_valid){
+					//If valid, send message to server
+					out.println(message);
+					}
+				else{System.out.println("Input has invalid form");}
+				
+			}
+			
+           out.println(message);
+           System.out.println(in.readLine());
+			
         }
     }
+	
+	public static boolean CheckMessage(Scanner msg_scan){
+		boolean valid = false;
+		try{
+		String type = msg_scan.next();
+		int coord_x;
+		int coord_y;
+		int width;
+		int height;
+		String color = "";
+		String text = "";
+		if(type.equals("POST")){
+			coord_x = msg_scan.nextInt();
+			coord_y = msg_scan.nextInt();
+			width = msg_scan.nextInt();
+			height = msg_scan.nextInt();
+			color = msg_scan.next();
+			text = msg_scan.next();
+			if(coord_x >= 0 && coord_y >= 0 && width > 0 && height >0 && color != null && text !=null){
+				valid = true;
+			}
+		}
+		else if(type.equals("GET")){
+			text = msg_scan.next();
+			if(text != null){
+			valid = true;
+			}
+		}
+		else if(type.equals("PIN")){
+			coord_x = msg_scan.nextInt();
+			coord_y = msg_scan.nextInt();
+			if(coord_x >= 0 && coord_y >= 0){
+				valid = true;
+			}
+		}
+		else if(type.equals("UNPIN")){
+			coord_x = msg_scan.nextInt();
+			coord_y = msg_scan.nextInt();
+			if(coord_x >= 0 && coord_y >= 0){
+				valid = true;
+			}
+		}
+		else if(type.equals("CLEAR")){
+			valid = true;
+		}
+		}
+		catch(Exception e){
+			//Errors mean the format is invalid
+		}
+		return valid;
+		
+		
+	}
 }
 
